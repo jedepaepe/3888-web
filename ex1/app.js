2a0681de12c90f31ab8111d02024b79f7cbcc65e
Depaepe JEAN-MICHEL <FHA576@win.corp.com>
2018-12-21 07:40:10 +0100

    static web server with a html file, for exemple

--- ex1/app.js
+++ ex1/app.js
@@ -15,15 +15,8 @@ var express = require('express');
 // idem pour librairie "path"
 var path = require('path');
 
-// idem pour librairie "cookie-parser"
-var cookieParser = require('cookie-parser');
-
 // idem pour librairie "morgan"
 var logger = require('morgan');
-
-// 1.2 chargement du "librairies" locales
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 // 2. la fonction express() construit un objet
 //    retourne la référence sur l'objet
@@ -43,13 +36,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
