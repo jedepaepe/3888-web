d1566dafe6caab92661ec0f511c3afc0a5d8b511
Depaepe JEAN-MICHEL <FHA576@win.corp.com>
2018-12-21 16:49:19 +0100

    removing urlencoded

--- ex1/app.js
+++ ex1/app.js
@@ -36,7 +36,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
