bb42e8e894d2ff7907f17860994c8a018096d63f
Depaepe JEAN-MICHEL <FHA576@win.corp.com>
2019-01-10 17:21:59 +0100

    Exemple from book - simple API random

--- ex1/app.js
+++ ex1/app.js
@@ -1,75 +1,26 @@
-// 1. chargement des librairies
+// Express in Action
+// Evan M. Hahn
+// page 91
 
-// 1.1. chargement des librairies externes (récupérer "npm install" et installer dans nodes_modules)
+var express = require("express");
 
-// "require('http-errors')" charge la librairie "http-error" et
-// retourne une référence vers la librairies 'http-errors' et 
-// assigne cette référence à la variable 'createError'
-var createError = require('http-errors');
-
-// "require('express') charge la librairie "express" et 
-// retourne une référence vers cette librairie et 
-// assigne cette référence à la variable "express"
-var express = require('express');
-
-// idem pour librairie "path"
-var path = require('path');
-
-// idem pour librairie "cookie-parser"
-var cookieParser = require('cookie-parser');
-
-// idem pour librairie "morgan"
-var logger = require('morgan');
-
-// 1.2 chargement du "librairies" locales
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-// 2. la fonction express() construit un objet
-//    retourne la référence sur l'objet
-//    qui est sauvée dans la variable app
 var app = express();
 
-// 3. dire à app (l'application express) quel view-engine utiliser
-//    view = vue : c'est que voit le client/utilisateur
-//      client = browser
-//      view-engine construite la réponse (response) envoyée au brower
+app.get("/random/:min/:max", function(req, res) {
+  var min = parseInt(req.params.min);
+  var max = parseInt(req.params.max);
 
-// où sont les vues (views)
-//    rem: path.join(__dirname, 'views') retourne
-//            ./views sous Linux
-//            .\views sous Windows
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.post("/submit", function(req, res) {
-  console.log(req.body.username);
-  res.send("Hello " + req.body.username);
+  if(isNaN(min) || isNaN(max)) {
+    res.status(400);
+    res.json({error: "Bad request"});
+    return;
+  }
+  
+  var result = Math.round(Math.random() * (max - min) + min);
+  res.json({result: result});
 });
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+  
+app.listen(3000, function() {
+  console.log("Random API app started");
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
