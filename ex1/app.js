7a4374bd4f8713f13c03a6198653070623167a2a
Depaepe JEAN-MICHEL <FHA576@win.corp.com>
2019-01-25 14:12:38 +0100

    readability improvements

--- ex1/app.js
+++ ex1/app.js
@@ -9,24 +9,23 @@ var app = express();
 
 app.use(logger('dev'));
 
-// API logger
+// return "hello" to HTTP request GET on url http://localhost/hello
 app.get("/hello", function(req, res) {
   res.send("hello");
 });
 
 // favicon https://stackoverflow.com/questions/15463199/how-to-set-custom-favicon-in-express
- //const fs = require('fs'); 
- //const favicon = fs.readFileSync(__dirname+'/public/favicon.ico'); // read file
- const favicon = new Buffer('AAABAAEAEBAQAAAAAAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA/4QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEREQAAAAAAEAAAEAAAAAEAAAABAAAAEAAAAAAQAAAQAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAEAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//wAA//8AAP//AAD8HwAA++8AAPf3AADv+wAA7/sAAP//AAD//wAA+98AAP//AAD//wAA//8AAP//AAD//wAA', 'base64'); 
- app.get("/favicon.ico", function(req, res) {
+//const fs = require('fs'); 
+//const favicon = fs.readFileSync(__dirname+'/public/favicon.ico'); // read file
+const favicon = new Buffer('AAABAAEAEBAQAAAAAAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA/4QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEREQAAAAAAEAAAEAAAAAEAAAABAAAAEAAAAAAQAAAQAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAEAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//wAA//8AAP//AAD8HwAA++8AAPf3AADv+wAA7/sAAP//AAD//wAA+98AAP//AAD//wAA//8AAP//AAD//wAA', 'base64'); 
+app.get("/favicon.ico", function(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Length', favicon.length);
   res.setHeader('Content-Type', 'image/x-icon');
   res.setHeader("Cache-Control", "public, max-age=2592000");                // expiers after a month
   res.setHeader("Expires", new Date(Date.now() + 2592000000).toUTCString());
   res.end(favicon);
- });
-
+});
   
 app.listen(3000, function() {
   console.log("Hello app started");
